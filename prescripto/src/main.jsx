ecf6ea350ca65615354ade340da1be320273b231
ryalee <sodre100ryan@gmail.com>
2024-11-07 20:59:43 -0300

    routes update

--- prescripto/src/main.jsx
+++ prescripto/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppContextProvider from './context/AppContext.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
